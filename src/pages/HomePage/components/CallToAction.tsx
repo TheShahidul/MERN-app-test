c9fc1d70edeed32dd3a4457d76fd8fa8df316325
Md. Shahidul Islam Prodhan <theshahidul.edu@gmail.com>
2025-09-02 14:13:00 +0600

    updated call to action

--- src/pages/HomePage/components/CallToAction.tsx
+++ src/pages/HomePage/components/CallToAction.tsx
@@ -7,7 +7,7 @@ const CallToAction: React.FC = () => (
         className="relative bg-cover bg-center text-white p-16 md:p-24"
         style={{ backgroundImage: `url(${Aarong1})` }}
     >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0" style={{background: 'linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,1))'}}></div>
         <div className="relative z-10 flex flex-col items-center gap-y-4">
             <h2 className="text-xl md:text-3xl font-bold max-w-4xl mx-auto">
                 Discover Unique Pieces to Beautify Your Home – Your Ultimate Destination for Home Decor!
